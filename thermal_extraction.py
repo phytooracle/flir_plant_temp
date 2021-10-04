a9e1e6c210f83ec899699f62ea8203d2326c3e3a
Emmanuel Gonzalez <emmanuelgonzalez@email.arizona.edu>
2021-10-03 22:00:28 -0700

    Update thermal_extraction.py

--- thermal_extraction.py
+++ thermal_extraction.py
@@ -78,7 +78,7 @@ def get_args():
                         help='Number of CPUs for multiprocessing',
                         metavar='cpu',
                         type=int,
-                        required=True)
+                        required=False)
 
     parser.add_argument('-of',
                         '--outfile',
@@ -343,7 +343,7 @@ def main():
     img_list = get_paths(args.dir)
     major_df = pd.DataFrame()
 
-    with multiprocessing.Pool(args.cpu) as p:
+    with multiprocessing.Pool(multiprocessing.cpu_count()) as p:
         df = p.map(process_image, img_list)
         major_df = major_df.append(df)
 
